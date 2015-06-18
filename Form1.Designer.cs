63514da5a5154c417f008f423f59a420f8eba948
Quintin <quintin.evans@accesso.com>
2015-06-18 12:33:47 -0400

    Data Validation Tool: Validates whether app Configs correct so Merchants for POS can print to boca

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DataValidationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.validateFunctionalityBtn = new System.Windows.Forms.Button();
+            this.merchantToCheck = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(45, 32);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(205, 127);
+            this.outputBox.TabIndex = 0;
+            this.outputBox.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.checkBox1.Location = new System.Drawing.Point(267, 32);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Print Boca";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // validateFunctionalityBtn
+            // 
+            this.validateFunctionalityBtn.Location = new System.Drawing.Point(267, 237);
+            this.validateFunctionalityBtn.Name = "validateFunctionalityBtn";
+            this.validateFunctionalityBtn.Size = new System.Drawing.Size(75, 23);
+            this.validateFunctionalityBtn.TabIndex = 2;
+            this.validateFunctionalityBtn.Text = "Validate Functionality";
+            this.validateFunctionalityBtn.UseVisualStyleBackColor = true;
+            this.validateFunctionalityBtn.Click += new System.EventHandler(this.onValidateFunctionalityBtnClick);
+            // 
+            // merchantToCheck
+            // 
+            this.merchantToCheck.Location = new System.Drawing.Point(45, 240);
+            this.merchantToCheck.Name = "merchantToCheck";
+            this.merchantToCheck.Size = new System.Drawing.Size(148, 20);
+            this.merchantToCheck.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Desired Merchant To Validate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 287);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.merchantToCheck);
+            this.Controls.Add(this.validateFunctionalityBtn);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.outputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button validateFunctionalityBtn;
+        private System.Windows.Forms.TextBox merchantToCheck;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace DataValidationApp
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection connection;
+        private String server;
+        private String port;
+        private String database;
+        private String uid;
+        private String password;
+        private String connectionString;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            server = "192.168.2.246";
+            port = "3306";
+            database = "ticketing_spaceneedle_test";
+            uid = "dba";
+            password = "sql";
+
+            connectionString = "SERVER=" + server + ";" + "PORT=" + port + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+            if(OpenConnection())
+            {
+                outputBox.AppendText("Successful Connect" + Environment.NewLine);
+            }
+            else 
+            {
+                outputBox.AppendText("Failed Connect" + Environment.NewLine);
+            }
+           
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+
+                MessageBox.Show(ex.Message + ex.StackTrace);                
+                return false;
+            }
+        }
+
+        private void onValidateFunctionalityBtnClick(object sender, EventArgs e)
+        {
+            //If Print Boca Checked
+            if (checkBox1.Checked)
+            {
+                //Validate App Configs
+                //app Id for Passport POS
+                String sql = "SELECT a.id, a.description, a.name FROM application a WHERE a.description LIKE '%POS%' OR a.name LIKE '%POS%'";
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader results = cmd.ExecuteReader();
+
+                List<String> posApplicationIds = new List<string>();
+
+                while(results.Read())
+                {
+                    posApplicationIds.Add(results["id"] + "");
+                }
+
+                results.Close();
+
+                String sqlToFindAppConfigs = "select ac.merchant_id, m.display_name, ac.name, ac.value from application_config ac join merchant m on ac.merchant_id = m.merchant_id where ac.application_id = @param_val_1 and ac.name = 'sendTicketsToBoca' and ac.value = 'true'";
+                //String sqlToFindAppConfigs = "select ac.merchant_id, m.display_name, ac.name, ac.value from application_config ac join merchant m on ac.merchant_id = m.merchant_id where ac.application_id = @param_val_1 and ac.name in ('sendTicketsToBoca','groupItemPrinting')";
+
+                bool anyBadResults = false;
+
+                for (int index = 0; index < posApplicationIds.Count; index++)
+                {
+                    MySqlCommand cmdForAppConfigs = new MySqlCommand(sqlToFindAppConfigs, connection);
+                    cmdForAppConfigs.Parameters.AddWithValue("@param_val_1", posApplicationIds[index]);
+                    MySqlDataReader rs = cmdForAppConfigs.ExecuteReader();
+                    List<String> merchantIdsWithBocaEnabled = new List<string>();
+                    while(rs.Read())
+                    {
+                        merchantIdsWithBocaEnabled.Add(rs["merchant_id"] + "");                        
+                    }
+                    rs.Close();
+                    for (int merchantIndex = 0; merchantIndex < merchantIdsWithBocaEnabled.Count; merchantIndex++)
+                    {
+                        String sqlForGroupPrintConfig = "select ac.merchant_id, m.display_name, ac.name, ac.value from application_config ac join merchant m on ac.merchant_id = m.merchant_id where ac.application_id = @param_val_1 and ac.name = 'groupItemPrinting' and ac.value = 'true' and ac.merchant_id = @param_val_2";
+                        //validate that each one DOESNT have groupItemPrinting set to true
+                        MySqlCommand cmdForGroupPrintingConfig = new MySqlCommand(sqlForGroupPrintConfig, connection);
+                        cmdForGroupPrintingConfig.Parameters.AddWithValue("@param_val_1", posApplicationIds[index]);
+                        cmdForGroupPrintingConfig.Parameters.AddWithValue("@param_val_2", merchantIdsWithBocaEnabled[merchantIndex]);
+                        
+                        MySqlDataReader badResults = cmdForGroupPrintingConfig.ExecuteReader();
+                        
+                        if(badResults.Read())
+                        {
+                            outputBox.AppendText("For Merchant ID " + merchantIdsWithBocaEnabled[merchantIndex] + "change application config values in application_config table for sendTicketsToBoca to true and groupItemPrinting to false!" + Environment.NewLine);
+                            anyBadResults = true;
+                        }
+
+                        badResults.Close();
+                    }
+                }
+
+                if(!anyBadResults)
+                {
+                    outputBox.AppendText("All merchants Enabled For Boca Printing!!" + Environment.NewLine);
+                }
+                
+                //Validate Print Formats Exist
+
+                //Make sure all active packages for POS merchants have valid Boca Formats
+            }
+            
+
+        }
+
+        private void printList(List<String> listToPrint)
+        {
+            for (int index = 0; index < listToPrint.Count; index++)
+            {
+                outputBox.AppendText(listToPrint[index]);
+            }
+        }
+
+    }
+
+}
